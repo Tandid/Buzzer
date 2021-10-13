e6eb63d9d977dc2ea9897fbce024680b81d62afd
Tandid Alam <tandid.alam@gmail.com>
2021-10-13 00:16:17 -0400

    added login page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const middleware = require("./middleware");
 
 const server = app.listen(port, () =>
   console.log(`Server listening on port ${port}`)
@@ -9,7 +10,10 @@ const server = app.listen(port, () =>
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.get("/", (req, res, next) => {
+const loginRoute = require("./routes/loginRoutes");
+app.use("/login", loginRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
   const payload = {
     pageTitle: "Home",
   };
