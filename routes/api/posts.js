83a2c43d113ce43b776cedb6d1df899f84b40eac
Tandid Alam <tandid.alam@gmail.com>
2021-10-23 19:12:36 -0400

    posts showing up

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,18 +3,27 @@ const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
 const User = require("../../schemas/UserSchema");
-const Post = require("../../schemas/postSchema");
+const Post = require("../../schemas/PostSchema");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get("/", (req, res, next) => {});
+router.get("/", (req, res, next) => {
+  Post.find()
+    .populate("postedBy")
+    .then((results) => res.status(200).send(results))
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(400);
+    });
+});
 
 router.post("/", async (req, res, next) => {
   if (!req.body.content) {
     console.log("Content param not sent with request");
     return res.sendStatus(400);
   }
-  let postData = {
+
+  var postData = {
     content: req.body.content,
     postedBy: req.session.user,
   };
@@ -22,6 +31,7 @@ router.post("/", async (req, res, next) => {
   Post.create(postData)
     .then(async (newPost) => {
       newPost = await User.populate(newPost, { path: "postedBy" });
+
       res.status(201).send(newPost);
     })
     .catch((error) => {
