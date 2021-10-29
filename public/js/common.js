b3b51a30ce688d595d9e7efdd591a762845cc69c
Tandid Alam <tandid.alam@gmail.com>
2021-10-29 19:51:35 -0400

    like button

--- public/js/common.js
+++ public/js/common.js
@@ -30,6 +30,27 @@ $("#submitPostButton").click(() => {
   });
 });
 
+$(document).on("click", ".likeButton", (event) => {
+  var button = $(event.target);
+  var postId = getPostIdFromElement(button);
+  if (postId === undefined) return;
+  $.ajax({
+    url: "/api/posts",
+    type: "PUT",
+    success: (postData) => {
+      console.log(postData);
+    },
+  });
+});
+
+function getPostIdFromElement(element) {
+  var isRoot = element.hasClass("post");
+  var rootElement = isRoot ? element : element.closest(".post");
+  var postId = rootElement.data().id;
+  if (postId === undefined) return alert("Post id undefined");
+  return postId;
+}
+
 function createPostHtml(postData) {
   var postedBy = postData.postedBy;
 
@@ -40,7 +61,7 @@ function createPostHtml(postData) {
   var displayName = postedBy.firstName + " " + postedBy.lastName;
   var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
 
-  return `<div class='post'>
+  return `<div class='post' data-id='${postData._id}'>
 
                 <div class='mainContentContainer'>
                     <div class='userImageContainer'>
@@ -67,7 +88,7 @@ function createPostHtml(postData) {
                                 </button>
                             </div>
                             <div class='postButtonContainer'>
-                                <button>
+                                <button class="likeButton">
                                     <i class='far fa-heart'></i>
                                 </button>
                             </div>
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 router.get("/", (req, res, next) => {
   Post.find()
     .populate("postedBy")
+    .sort({ createdAt: -1 })
     .then((results) => res.status(200).send(results))
     .catch((error) => {
       console.log(error);
@@ -38,6 +39,10 @@ router.post("/", async (req, res, next) => {
       console.log(error);
       res.sendStatus(400);
     });
+});
+
+router.put("/", async (req, res, next) => {
+  res.status(200).send("updated");
 });
 
 module.exports = router;
