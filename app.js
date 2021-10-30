ff1f2069347d47e3bc2bb16348bcef7d2c2a4a69
Tandid Alam <tandid.alam@gmail.com>
2021-10-29 23:39:34 -0400

    post page working

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require("./database");
 const session = require("express-session");
 
 const server = app.listen(port, () =>
-  console.log(`Server listening on port ${port}`)
+  console.log("Server listening on port " + port)
 );
 
 app.set("view engine", "pug");
@@ -16,6 +16,7 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(
   session({
     secret: "pizza",
@@ -24,25 +25,26 @@ app.use(
   })
 );
 
-//Routes
+// Routes
 const loginRoute = require("./routes/loginRoutes");
 const registerRoute = require("./routes/registerRoutes");
-const logoutRoute = require("./routes/logout");
+const postRoute = require("./routes/postRoutes");
 
-//API Routes
-const postApiRoute = require("./routes/api/posts");
+// Api routes
+const postsApiRoute = require("./routes/api/posts");
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
-app.use("/logout", logoutRoute);
+app.use("/posts", middleware.requireLogin, postRoute);
 
-app.use("/api/posts", postApiRoute);
+app.use("/api/posts", postsApiRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
-  const payload = {
+  var payload = {
     pageTitle: "Home",
     userLoggedIn: req.session.user,
     userLoggedInJs: JSON.stringify(req.session.user),
   };
+
   res.status(200).render("home", payload);
 });
