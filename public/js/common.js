577fc4e2affdf871e2506c096b01aa8a98d83a7b
Tandid Alam <tandid.alam@gmail.com>
2021-10-30 05:00:39 -0400

    got cropper to start working

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,6 @@
+// Globals
+var cropper;
+
 $("#postTextarea, #replyTextarea").keyup((event) => {
   var textbox = $(event.target);
   var value = textbox.val().trim();
@@ -79,6 +82,28 @@ $("#deletePostButton").click((event) => {
       location.reload();
     },
   });
+});
+
+$("#filePhoto").change(function () {
+  if (this.files && this.files[0]) {
+    var reader = new FileReader();
+    reader.onload = (e) => {
+      var image = document.getElementById("imagePreview");
+      image.src = e.target.result;
+
+      if (cropper !== undefined) {
+        cropper.destroy();
+      }
+
+      cropper = new Cropper(image, {
+        aspectRatio: 1 / 1,
+        background: false,
+      });
+    };
+    reader.readAsDataURL(this.files[0]);
+  } else {
+    console.log("nope");
+  }
 });
 
 $(document).on("click", ".likeButton", (event) => {
@@ -204,9 +229,9 @@ function createPostHtml(postData, largeFont = false) {
   var retweetText = "";
   if (isRetweet) {
     retweetText = `<span>
-                      <i class='fas fa-retweet'></i>
-                      Retweeted by <a href='/profile/${retweetedBy}'>@${retweetedBy}</a>    
-                  </span>`;
+                        <i class='fas fa-retweet'></i>
+                        Retweeted by <a href='/profile/${retweetedBy}'>@${retweetedBy}</a>    
+                    </span>`;
   }
 
   var replyFlag = "";
@@ -219,8 +244,8 @@ function createPostHtml(postData, largeFont = false) {
 
     var replyToUsername = postData.replyTo.postedBy.username;
     replyFlag = `<div class='replyFlag'>
-                      Replying to <a href='/profile/${replyToUsername}'>@${replyToUsername}<a>
-                  </div>`;
+                        Replying to <a href='/profile/${replyToUsername}'>@${replyToUsername}<a>
+                    </div>`;
   }
 
   var buttons = "";
@@ -229,50 +254,50 @@ function createPostHtml(postData, largeFont = false) {
   }
 
   return `<div class='post ${largeFontClass}' data-id='${postData._id}'>
-              <div class='postActionContainer'>
-                  ${retweetText}
-              </div>
-              <div class='mainContentContainer'>
-                  <div class='userImageContainer'>
-                      <img src='${postedBy.profilePic}'>
-                  </div>
-                  <div class='postContentContainer'>
-                      <div class='header'>
-                          <a href='/profile/${
-                            postedBy.username
-                          }' class='displayName'>${displayName}</a>
-                          <span class='username'>@${postedBy.username}</span>
-                          <span class='date'>${timestamp}</span>
-                          ${buttons}
-                      </div>
-                      ${replyFlag}
-                      <div class='postBody'>
-                          <span>${postData.content}</span>
-                      </div>
-                      <div class='postFooter'>
-                          <div class='postButtonContainer'>
-                              <button data-toggle='modal' data-target='#replyModal'>
-                                  <i class='far fa-comment'></i>
-                              </button>
-                          </div>
-                          <div class='postButtonContainer green'>
-                              <button class='retweetButton ${retweetButtonActiveClass}'>
-                                  <i class='fas fa-retweet'></i>
-                                  <span>${
-                                    postData.retweetUsers.length || ""
-                                  }</span>
-                              </button>
-                          </div>
-                          <div class='postButtonContainer red'>
-                              <button class='likeButton ${likeButtonActiveClass}'>
-                                  <i class='far fa-heart'></i>
-                                  <span>${postData.likes.length || ""}</span>
-                              </button>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>`;
+                <div class='postActionContainer'>
+                    ${retweetText}
+                </div>
+                <div class='mainContentContainer'>
+                    <div class='userImageContainer'>
+                        <img src='${postedBy.profilePic}'>
+                    </div>
+                    <div class='postContentContainer'>
+                        <div class='header'>
+                            <a href='/profile/${
+                              postedBy.username
+                            }' class='displayName'>${displayName}</a>
+                            <span class='username'>@${postedBy.username}</span>
+                            <span class='date'>${timestamp}</span>
+                            ${buttons}
+                        </div>
+                        ${replyFlag}
+                        <div class='postBody'>
+                            <span>${postData.content}</span>
+                        </div>
+                        <div class='postFooter'>
+                            <div class='postButtonContainer'>
+                                <button data-toggle='modal' data-target='#replyModal'>
+                                    <i class='far fa-comment'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer green'>
+                                <button class='retweetButton ${retweetButtonActiveClass}'>
+                                    <i class='fas fa-retweet'></i>
+                                    <span>${
+                                      postData.retweetUsers.length || ""
+                                    }</span>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer red'>
+                                <button class='likeButton ${likeButtonActiveClass}'>
+                                    <i class='far fa-heart'></i>
+                                    <span>${postData.likes.length || ""}</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
 }
 
 function timeDifference(current, previous) {
