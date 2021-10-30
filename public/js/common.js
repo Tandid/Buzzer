3e337ec25a9c03db8a6dda8f8a9cbfed5ea25b6d
Tandid Alam <tandid.alam@gmail.com>
2021-10-30 02:00:52 -0400

    made some more changes to profile page, little details

--- public/js/common.js
+++ public/js/common.js
@@ -171,9 +171,9 @@ function createPostHtml(postData, largeFont = false) {
   var retweetText = "";
   if (isRetweet) {
     retweetText = `<span>
-                        <i class='fas fa-retweet'></i>
-                        Retweeted by <a href='/profile/${retweetedBy}'>@${retweetedBy}</a>    
-                    </span>`;
+                      <i class='fas fa-retweet'></i>
+                      Retweeted by <a href='/profile/${retweetedBy}'>@${retweetedBy}</a>    
+                  </span>`;
   }
 
   var replyFlag = "";
@@ -186,8 +186,8 @@ function createPostHtml(postData, largeFont = false) {
 
     var replyToUsername = postData.replyTo.postedBy.username;
     replyFlag = `<div class='replyFlag'>
-                        Replying to <a href='/profile/${replyToUsername}'>@${replyToUsername}<a>
-                    </div>`;
+                      Replying to <a href='/profile/${replyToUsername}'>@${replyToUsername}<a>
+                  </div>`;
   }
 
   var buttons = "";
@@ -196,50 +196,50 @@ function createPostHtml(postData, largeFont = false) {
   }
 
   return `<div class='post ${largeFontClass}' data-id='${postData._id}'>
-                <div class='postActionContainer'>
-                    ${retweetText}
-                </div>
-                <div class='mainContentContainer'>
-                    <div class='userImageContainer'>
-                        <img src='${postedBy.profilePic}'>
-                    </div>
-                    <div class='postContentContainer'>
-                        <div class='header'>
-                            <a href='/profile/${
-                              postedBy.username
-                            }' class='displayName'>${displayName}</a>
-                            <span class='username'>@${postedBy.username}</span>
-                            <span class='date'>${timestamp}</span>
-                            ${buttons}
-                        </div>
-                        ${replyFlag}
-                        <div class='postBody'>
-                            <span>${postData.content}</span>
-                        </div>
-                        <div class='postFooter'>
-                            <div class='postButtonContainer'>
-                                <button data-toggle='modal' data-target='#replyModal'>
-                                    <i class='far fa-comment'></i>
-                                </button>
-                            </div>
-                            <div class='postButtonContainer green'>
-                                <button class='retweetButton ${retweetButtonActiveClass}'>
-                                    <i class='fas fa-retweet'></i>
-                                    <span>${
-                                      postData.retweetUsers.length || ""
-                                    }</span>
-                                </button>
-                            </div>
-                            <div class='postButtonContainer red'>
-                                <button class='likeButton ${likeButtonActiveClass}'>
-                                    <i class='far fa-heart'></i>
-                                    <span>${postData.likes.length || ""}</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>`;
+              <div class='postActionContainer'>
+                  ${retweetText}
+              </div>
+              <div class='mainContentContainer'>
+                  <div class='userImageContainer'>
+                      <img src='${postedBy.profilePic}'>
+                  </div>
+                  <div class='postContentContainer'>
+                      <div class='header'>
+                          <a href='/profile/${
+                            postedBy.username
+                          }' class='displayName'>${displayName}</a>
+                          <span class='username'>@${postedBy.username}</span>
+                          <span class='date'>${timestamp}</span>
+                          ${buttons}
+                      </div>
+                      ${replyFlag}
+                      <div class='postBody'>
+                          <span>${postData.content}</span>
+                      </div>
+                      <div class='postFooter'>
+                          <div class='postButtonContainer'>
+                              <button data-toggle='modal' data-target='#replyModal'>
+                                  <i class='far fa-comment'></i>
+                              </button>
+                          </div>
+                          <div class='postButtonContainer green'>
+                              <button class='retweetButton ${retweetButtonActiveClass}'>
+                                  <i class='fas fa-retweet'></i>
+                                  <span>${
+                                    postData.retweetUsers.length || ""
+                                  }</span>
+                              </button>
+                          </div>
+                          <div class='postButtonContainer red'>
+                              <button class='likeButton ${likeButtonActiveClass}'>
+                                  <i class='far fa-heart'></i>
+                                  <span>${postData.likes.length || ""}</span>
+                              </button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>`;
 }
 
 function timeDifference(current, previous) {
