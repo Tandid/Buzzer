9650b36765ff4d2901ae9ba585ef6a9ff747d0a9
Tandid Alam <tandid.alam@gmail.com>
2021-10-30 16:53:01 -0400

    pinned post returns true and false

--- schemas/PostSchema.js
+++ schemas/PostSchema.js
@@ -4,22 +4,17 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema(
   {
-    content: {
-      type: String,
-      trim: true,
-    },
-    postedBy: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      pinned: Boolean,
-    },
+    content: { type: String, trim: true },
+    postedBy: { type: Schema.Types.ObjectId, ref: "User" },
+    pinned: Boolean,
     likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
     retweetUsers: [{ type: Schema.Types.ObjectId, ref: "User" }],
     retweetData: { type: Schema.Types.ObjectId, ref: "Post" },
     replyTo: { type: Schema.Types.ObjectId, ref: "Post" },
+    pinned: Boolean,
   },
   { timestamps: true }
 );
 
-let Post = mongoose.model("Post", PostSchema);
+var Post = mongoose.model("Post", PostSchema);
 module.exports = Post;
