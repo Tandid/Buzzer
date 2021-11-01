f9742dd60194ff9196fa3fcf07ce36852e8b64e7
Tandid Alam <tandid.alam@gmail.com>
2021-11-01 19:40:03 -0400

    getting some data for actual chat page

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const User = require("../schemas/UserSchema");
+const Chat = require("../schemas/ChatSchema");
 
 router.get("/", (req, res, next) => {
   res.status(200).render("inboxPage", {
@@ -18,6 +19,27 @@ router.get("/new", (req, res, next) => {
     pageTitle: "New message",
     userLoggedIn: req.session.user,
     userLoggedInJs: JSON.stringify(req.session.user),
+  });
+});
+
+router.get("/:chatId", async (req, res, next) => {
+  var userId = req.session.user._id;
+  var chatId = req.params.chatId;
+
+  var chat = await Chat.findOne({
+    _id: chatId,
+    users: { $elemMatch: { $eq: userId } },
+  }).populate("users");
+
+  if (chat == null) {
+    // Check if chat id is really user id
+  }
+
+  res.status(200).render("chatPage", {
+    pageTitle: "Chat",
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+    chat: chat,
   });
 });
 
