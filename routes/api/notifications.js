04e131dbda1294872d1780afb7d2fdb44cef46d3
Tandid Alam <tandid.alam@gmail.com>
2021-11-03 04:12:51 -0400

    notifications appear on page

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -11,7 +11,18 @@ const Notification = require("../../schemas/NotificationSchema");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", async (req, res, next) => {
-  res.status(200).send("working!");
+  Notification.find({
+    userTo: req.session.user._id,
+    notificationType: { $ne: "newMessage" },
+  })
+    .populate("userTo")
+    .populate("userFrom")
+    .sort({ createdAt: -1 })
+    .then((results) => res.status(200).send(results))
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(400);
+    });
 });
 
 module.exports = router;
