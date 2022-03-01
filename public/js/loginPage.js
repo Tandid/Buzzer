a6431841589d4a8d380d775597e8f4be7fb992a3
Tandid Alam <tandid.alam@gmail.com>
2022-03-01 17:57:31 -0500

    added demo login

---
+++ public/js/loginPage.js
@@ -0,0 +1,9 @@
+$(".demoLoginButton").click(() => {
+  autoCompleteLogin();
+});
+
+function autoCompleteLogin() {
+  //   $(".usernameInput").val()="tandid.alam@gmail.com"
+  //   $(".passwordInput").val()="Duck123"
+  console.log("Hello");
+}
